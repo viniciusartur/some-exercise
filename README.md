c30bd3166b319357ca5703c9c328465fbce25646
vinicius <viniciusartur@protonmail.com>
2023-11-09 13:44:51 +0000

    Some readme and enabled versioning

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # iofinnet-terraform
+
+```
+cd environments/dev
+terraform plan
+terraform apply
+```
+
+```
+echo bucket3 > /tmp/bucket3.txt
+aws s3 cp /tmp/bucket3.txt s3://xtesttest2x-bucket3-dev/customers/
+curl https://d3jz0esynvfnaz.cloudfront.net/customers/bucket3.txt
+```
--- terraform/modules/bucket/main.tf
+++ terraform/modules/bucket/main.tf
@@ -2,6 +2,13 @@ resource "aws_s3_bucket" "b" {
   bucket = var.bucket_name
 }
 
+resource "aws_s3_bucket_versioning" "versioning" {
+  bucket = aws_s3_bucket.b.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
 resource "aws_s3_bucket_policy" "cdn-oac-bucket-policy-b3" {
   bucket = aws_s3_bucket.b.id
   policy = data.aws_iam_policy_document.this.json
